efd251d9856093e80e3b62d5b1fc229814f033da
Saurabhrajmane-23 <iamsaurabhrajmane@gmail.com>
2024-08-02 17:17:59 +0530

    Connected to mongoDB database

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({
+   path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+;(async () => {
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+      app.on("error", (error) => {
+         console.log("ERROR: ", error);
+         throw error
+      })
+
+      app.listen(process.env.PORT, () => {
+         console.log(`Express connected successfully`);
+         console.log(`App is listening on port: ${process.env.PORT}`);
+      })
+
+   } catch (error) {
+      console.log("ERROR:", error);
+   }
+})();
+*/
